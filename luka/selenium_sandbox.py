5dd118708d0234a06d4a3a47eb79f14547f67b5c
Zefu Lu <shawnlu25@gmail.com>
2024-04-10 11:29:36 +0800

    ease out scroll

--- luka/selenium_sandbox.py
+++ luka/selenium_sandbox.py
@@ -5,11 +5,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import validators
+from time import sleep
 
 class SeleniumSandbox(object):
-    def __init__(self):
+    def __init__(self, window_size=(1024, 768)):
         self.driver = webdriver.Chrome()
-        self.driver.set_window_size(1024, 768)
+
+        self.window_size = window_size
+        self.driver.set_window_size(window_size[0], window_size[1])
 
     def visit(self, url):
         url = url if url.startswith("http") else "http://" + url
@@ -18,9 +21,56 @@ class SeleniumSandbox(object):
 
         self.driver.get(url)
 
+    def scroll(self, scroll_down=True, duration = 1000):
+        if scroll_down:
+            scroll_direction = "+"
+        else:
+            scroll_direction = "-"
 
+        completion_signal = "scroll_complete"
+        
+        ease_out_scroll_script = f"""
+        function smoothScroll(duration) {{
+            var startY = window.pageYOffset;
+            var endY = startY {scroll_direction} window.innerHeight;
+            var maxY = document.body.scrollHeight;
+
+            if (endY > maxY) {{
+                endY = maxY;
+            }} else if (endY < 0) {{
+                endY = 0;
+            }}
+
+            var diff = endY - startY;
+            var startTime = null;
+
+            function step(time) {{
+                if (startTime === null) startTime = time;
+                var t = time - startTime;
+                var percent = Math.min(1, t / duration);
+                var easing = 1 - Math.pow(1 - percent, 3);  
+                window.scrollTo(0, startY + diff * easing);
+                if (t < duration) {{
+                    window.requestAnimationFrame(step);
+                }} else {{
+                    document.body.setAttribute('data-{completion_signal}', 'true');
+                }}
+            }}
+            window.requestAnimationFrame(step);
+        }}
+        document.body.setAttribute('data-{completion_signal}', 'false');
+        smoothScroll({duration});
+        """
+        def check_scroll_complete(driver):
+            element = driver.find_element(By.TAG_NAME, "body")
+            return element.get_attribute(f"data-{completion_signal}") == "true"
+
+        self.driver.execute_script(ease_out_scroll_script)
+        WebDriverWait(self.driver, duration / 1000 + 2).until(
+            check_scroll_complete
+        )
+        
 if __name__ == "__main__":
     sandbox = SeleniumSandbox()
-    sandbox.visit("www.google.com")
     
 
