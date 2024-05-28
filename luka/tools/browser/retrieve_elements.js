5cddba456889177ee3c205249f6adb1484068f03
Zefu Lu <shawnlu25@gmail.com>
2024-05-28 09:41:22 +0800

    Added all possible tags

--- luka/tools/browser/retrieve_elements.js
+++ luka/tools/browser/retrieve_elements.js
@@ -1,5 +1,27 @@
 const selectedElements = [];
 
+// HTML tag categories from https://www.w3schools.com/tags/ref_byfunc.asp
+const TAGS_PROGRAMMING = "script, noscript, applet, embed, object, param";
+const TAGS_META = "head, meta, base, basefont"
+const TAGS_SEMANTICS = "style, div, span, header, hgroup, footer, main, section, search, article, aside, details, dialog, summary, data"
+const TAGS_TABLE = "table, caption, th, tr, td, thead, tbody, tfoot, col, colgroup"
+const TAGS_LISTS = "menu, ol, ul, li, dir, dl, dt, dd"
+const TAGS_LINKS = "a, link, nav"
+const TAGS_AV = "audio, video, source, track"
+const TAGS_IMAGES = "img, map, area, canvas, figcaption, figure, picture, svg"
+const TAGS_FRAMES = "iframe"
+const TAGS_INPUT = "form, input, textarea, button, select, optgroup, option, label, fieldset, legend, datalist, output"
+const TAGS_FORMAT = "abbr, address, b, bdi, bdo, blockquote, cite, code, del, dfn, em, i, ins, kbd, mark, meter, pre, progress, q, rp, rt, ruby, s, samp, small, strong, strong, sub, sup, template, time, u, var, wbr"
+const TAGS_BASIC = "html, head, title, body" 
+const TAGS_CONTENT = "h1, h2, h3, h4, h5, h6, p, br, hr"
+
+const isPureText = (element) => {
+    if (element.matches("a, link, nav, button, input, textarea, select")) {
+        return false;
+    }
+
+}
+
 const selectElement = (element) => {
     if (element.parentElement && element.parentElement.hasAttribute("luka-skip-element")) {
         element.setAttribute("luka-skip-element", "true");
@@ -16,17 +38,14 @@ const selectElement = (element) => {
         //if (element.contentWindow && element.contentWindow.origin === window.origin) {
         //    element.contentWindow.document.querySelectorAll('*').forEach(selectElement);
         //}
-    } else if (
-        element.matches("a, button, input[type=button], input[type=submit], input[type=reset], [role=\"button\"]") || 
-        (element.tagName.toLowerCase() === 'div' && element.onclick)
-    ) {
+    } else if (element.matches("a, link, nav, button, input[type=button], input[type=submit], input[type=reset], [role=\"button\"]")) {
         tag = "link",
         text = element.innerText.trim();
         if (text && text.length >= 0) {
             element.setAttribute("luka-skip-element", "true");
         }
 
-    } else if (element.matches("p, h1, h2, h3, h4, h5, h6, span, div, label, option")) {
+    } else if (element.matches("p, h1, h2, h3, h4, h5, h6, span, div, label, option, ul, li")) {
         if (!Array.from(element.childNodes).some(node => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '')) {
             return;
         }
